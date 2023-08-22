1bf70aab90eb4bfee7cb56584c40bbd17455b6df
Washington-Kimani <kimaniwashington39@gmail.com>
2023-08-22 09:05:44 +0300

    Some Changes

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,18 +1,22 @@
 import * as FaIcons from 'react-icons/fa';
+import * as CiIcons from 'react-icons/ci';
 import "./cards.css";
 
-const Cards = ({ name, image, location,whatsapp, github, twitter }) => {
+const Cards = ({ name, image, location,whatsapp, github, linkedin }) => {
   return (
     <div className="card-container">
       <span className="pro">PRO</span>
       <img className="round" src={image} alt="user"/>
       <h3>{name}</h3>
-      <h6> {location}</h6>
+      <div className="location">
+        <CiIcons.CiLocationOn className='location_icon'/>
+        <h6>{location}</h6>
+      </div>
       <p>User interface designer and <br /> front-end developer.</p>
       <div className="socials">
         <a href={whatsapp} target='_blank'><FaIcons.FaWhatsapp className='social_icon'/></a>
         <a href={github} target='_blank'><FaIcons.FaGithub className='social_icon'/></a>
-        <a href={twitter} target='_blank'><FaIcons.FaTwitter className='social_icon'/></a>
+        <a href={linkedin} target='_blank'><FaIcons.FaLinkedin className='social_icon'/></a>
       </div>
       <div className="skills">
         <h6>Skills</h6>
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -10,7 +10,7 @@ const props = [
     location: "Nairobi",
     whatsapp: "https://wa.me/+254757909044?text=Hello%20John",
     github: "https://github.com/Mwatheka",
-    twitter: "https://twitter.com/",
+    linkedin: "https://www.linkedin.com/in/abraham-mwatheka-424250239",
   },
   {
     name: "Alexander Maina",
@@ -18,7 +18,7 @@ const props = [
     location: "Nairobi",
     whatsapp: "https://wa.me/+254768444197?text=Hello%20Alex",
     github: "https://github.com/Alekiie",
-    twitter: "https://twitter.com/",
+    linkedin: "https://www.linkedin.com/in/alexander-maina-63ba8a232",
   },
   {
     name: "Washington Kimani",
@@ -26,24 +26,24 @@ const props = [
     location: "Nairobi",
     whatsapp: "https://wa.me/+254745811535?text=Hello%20Washington",
     github: "https://github.com/Washington-Kimani",
-    twitter: "https://twitter.com/",
+    linkedin: "https://www.linkedin.com/in/washington-kimani-4a306b232/",
   },
   {
-    name: "Joseph Junior",
+    name: "Joseph Odira",
     image: Avatar,
     location: "Nairobi",
     whatsapp: "https://wa.me/+254112858989?text=Hello%20Joseph",
     github: "https://github.com/",
-    twitter: "https://twitter.com/",
+    linkedin: "https://ke.linkedin.com/in/joseph-odira-52b283225",
   },
   {
-    name: "Nick Teck",
+    name: "Nicholas Murimi",
     image: Avatar,
     location: "Nairobi",
     whatsapp: "https://wa.me/+254716453748?text=Hello%20Nick",
     github: "https://github.com/",
-    twitter: "https://twitter.com/",
-  }
+    linkedin: "https://www.linkedin.com/in/nicholas-murimi-684113233",
+  },
 ];
 
 const Team = () => {
@@ -63,11 +63,11 @@ const Team = () => {
           at a time.
         </p>
         <div className="flex flex-col md:grid md:grid-cols-3 gap-8 mt-8">
-          <Cards name={props[0].name} image={props[0].image} location={props[0].location} whatsapp={props[0].whatsapp} github={props[0].github} twitter={props[0].twitter}/>
-          <Cards name={props[1].name} image={props[1].image} location={props[1].location} whatsapp={props[1].whatsapp} github={props[1].github} twitter={props[1].twitter}/>
-          <Cards name={props[2].name} image={props[2].image} location={props[2].location} whatsapp={props[2].whatsapp} github={props[2].github} twitter={props[2].twitter}/>
-          <Cards name={props[3].name} image={props[3].image} location={props[3].location} whatsapp={props[3].whatsapp} github={props[3].github} twitter={props[3].twitter}/>
-          <Cards name={props[4].name} image={props[4].image} location={props[4].location} whatsapp={props[4].whatsapp} github={props[4].github} twitter={props[4].twitter}/>
+          <Cards name={props[0].name} image={props[0].image} location={props[0].location} whatsapp={props[0].whatsapp} github={props[0].github} linkedin={props[0].linkedin}/>
+          <Cards name={props[1].name} image={props[1].image} location={props[1].location} whatsapp={props[1].whatsapp} github={props[1].github} linkedin={props[1].linkedin}/>
+          <Cards name={props[2].name} image={props[2].image} location={props[2].location} whatsapp={props[2].whatsapp} github={props[2].github} linkedin={props[2].linkedin}/>
+          <Cards name={props[3].name} image={props[3].image} location={props[3].location} whatsapp={props[3].whatsapp} github={props[3].github} linkedin={props[3].linkedin}/>
+          <Cards name={props[4].name} image={props[4].image} location={props[4].location} whatsapp={props[4].whatsapp} github={props[4].github} linkedin={props[4].linkedin}/>
         </div>
       </div>
       <Footer />
