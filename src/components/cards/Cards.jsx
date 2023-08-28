5157db3ac71bbdab349dbe37fa90167f4059e863
Washington Kimani <kimaniwashington39@gmail.com>
2023-08-28 09:56:12 +0300

    Added Title and Skills

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -2,32 +2,29 @@ import * as FaIcons from 'react-icons/fa';
 import * as CiIcons from 'react-icons/ci';
 import "./cards.css";
 
-const Cards = ({ name, image, location,whatsapp, github, linkedin }) => {
+
+const Cards = ({data}) => {
   return (
     <div className="card-container">
       <span className="pro">PRO</span>
-      <img className="round" src={image} alt="user"/>
-      <h3>{name}</h3>
+      <img className="round" src={data.image} alt="user"/>
+      <h3>{data.name}</h3>
       <div className="location">
         <CiIcons.CiLocationOn className='location_icon'/>
-        <h6>{location}</h6>
+        <h6>{data.location}</h6>
       </div>
-      <p>User interface designer and <br /> front-end developer.</p>
+      <p>{data.role}</p>
       <div className="socials">
-        <a href={whatsapp} target='_blank'><FaIcons.FaWhatsapp className='social_icon'/></a>
-        <a href={github} target='_blank'><FaIcons.FaGithub className='social_icon'/></a>
-        <a href={linkedin} target='_blank'><FaIcons.FaLinkedin className='social_icon'/></a>
+        <a href={data.whatsapp} target='_blank'><FaIcons.FaWhatsapp className='social_icon'/></a>
+        <a href={data.github} target='_blank'><FaIcons.FaGithub className='social_icon'/></a>
+        <a href={data.linkedin} target='_blank'><FaIcons.FaLinkedin className='social_icon'/></a>
       </div>
       <div className="skills">
         <h6>Skills</h6>
         <ul>
-          <li>UI / UX</li>
-          <li>Front End Development</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>Node</li>
+          {data.skills.map((skill, index)=>(
+            <li key={index}>{skill}</li>
+          ))}
         </ul>
       </div>
     </div>
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -11,6 +11,8 @@ const props = [
     whatsapp: "https://wa.me/+254757909044?text=Hello%20John",
     github: "https://github.com/Mwatheka",
     linkedin: "https://www.linkedin.com/in/abraham-mwatheka-424250239",
+    role : 'Full Stack Developer',
+    skills: ['JS', 'PHP', 'Laravel', 'API integration', 'HTML', 'CSS', 'WordPress']
   },
   {
     name: "Alexander Maina",
@@ -19,6 +21,8 @@ const props = [
     whatsapp: "https://wa.me/+254768444197?text=Hello%20Alex",
     github: "https://github.com/Alekiie",
     linkedin: "https://www.linkedin.com/in/alexander-maina-63ba8a232",
+    role : 'FrontEnd Developer',
+    skills: ['HTML','CSS','JS', 'MySQL', 'ReactJs', 'TailwindCSS', 'Nextjs','Nodejs','MongoDB','Python3']
   },
   {
     name: "Washington Kimani",
@@ -27,6 +31,8 @@ const props = [
     whatsapp: "https://wa.me/+254745811535?text=Hello%20Washington",
     github: "https://github.com/Washington-Kimani",
     linkedin: "https://www.linkedin.com/in/washington-kimani-4a306b232/",
+    role : 'MERN Stack Developer',
+    skills: ['HTML', 'CSS', 'JS', 'jQuery', 'BootStrap', 'MERN', 'MySQL']
   },
   {
     name: "Joseph Odira",
@@ -35,6 +41,8 @@ const props = [
     whatsapp: "https://wa.me/+254112858989?text=Hello%20Joseph",
     github: "https://github.com/",
     linkedin: "https://ke.linkedin.com/in/joseph-odira-52b283225",
+    role : 'FrontEnd Developer',
+    skills: ['JS', 'PHP', 'Laravel', 'API integration', 'HTML', 'CSS', 'WordPress']
   },
   {
     name: "Nicholas Murimi",
@@ -43,6 +51,8 @@ const props = [
     whatsapp: "https://wa.me/+254716453748?text=Hello%20Nick",
     github: "https://github.com/nic3holas",
     linkedin: "https://www.linkedin.com/in/nicholas-murimi-684113233",
+    role : 'Full Stack Developer',
+    skills: ['HTML','CSS', 'BootStrap','Angular','PHP','MySQL', 'JavaScript','Java','C++', 'Python 3', 'FrontEnd', 'Backend', 'Android Dev']
   },
 ];
 
@@ -63,11 +73,11 @@ const Team = () => {
           at a time.
         </p>
         <div className="flex flex-col md:grid md:grid-cols-3 gap-8 mt-8">
-          <Cards name={props[0].name} image={props[0].image} location={props[0].location} whatsapp={props[0].whatsapp} github={props[0].github} linkedin={props[0].linkedin}/>
-          <Cards name={props[1].name} image={props[1].image} location={props[1].location} whatsapp={props[1].whatsapp} github={props[1].github} linkedin={props[1].linkedin}/>
-          <Cards name={props[2].name} image={props[2].image} location={props[2].location} whatsapp={props[2].whatsapp} github={props[2].github} linkedin={props[2].linkedin}/>
-          <Cards name={props[3].name} image={props[3].image} location={props[3].location} whatsapp={props[3].whatsapp} github={props[3].github} linkedin={props[3].linkedin}/>
-          <Cards name={props[4].name} image={props[4].image} location={props[4].location} whatsapp={props[4].whatsapp} github={props[4].github} linkedin={props[4].linkedin}/>
+          <Cards data={props[0]} />
+          <Cards data={props[1]} />
+          <Cards data={props[2]} />
+          <Cards data={props[3]} />
+          <Cards data={props[4]} />
         </div>
       </div>
       <Footer />
